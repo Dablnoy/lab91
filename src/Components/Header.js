e1f7d733b3792030c1a7172d89c40413ee941154
Dablnoy <go.8761@mail.ru>
2022-04-08 09:09:57 +0600

    first commit2

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,25 @@ import "./Header.css"
 function Header(){
     return(
         <div className="Header">
-        <h2>asd</h2>
+          <div className="line">
+         
+           <div className="picture">
+             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_aHQmHh7mPmJwhiBTKqRs1P_Ze7-UD34cRg&usqp=CAU" alt="go"/>
+             
+           </div>
+           
+           <h2 className="text first">Продукты</h2>
+           <h2 className="text second">О нас</h2>
+           <h3 className="text third">Советы по безопасности</h3>
+           <h2 className="text four">Поддержка</h2>
+           <h2 className="text five">Установить</h2>
+           <h2 className="text six">Русский</h2>
+           <input className="but2" type="button" value="Создать аккаунт"/>
+           <input className="but" type="button" value="Войдите"/>
+           <h1 className="seven">Свайп вправа</h1>
+           
+          </div>
+          
         </div>
     );
 }
